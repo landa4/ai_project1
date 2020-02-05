e07f1d972b5d3d072591f7c5f24bc25935ad8de0
landa4 <player.961@gmx.de>
2020-02-05 12:05:13 +0100

    -AlphaBeta mit Root for testing

---
+++ src/AlphaBeta.java
@@ -0,0 +1,50 @@
+import java.util.List;
+
+public class AlphaBeta {
+
+    private Environment env;
+    private Heuristics heuristics;
+
+    public AlphaBeta(Environment e, Heuristics h){
+        env = e;
+        heuristics = h;
+    }
+
+    public Action alphaBetaRoot(int depth, State s, float alpha, float beta){
+        float bestValue = Float.NEGATIVE_INFINITY;
+        Action bestAction  = null;
+
+        List<Action> actions = env.get_legal_actions(s);
+        for(Action a : actions){
+            s.doAction(a, true); // do action
+            float value = -alphaBeta(depth-1,s, -beta, -alpha);
+            s.doAction(a,false); // undo action
+            if(value > bestValue){
+                bestValue = value;
+                bestAction = a;
+            }
+        }
+        return bestAction;
+    }
+
+    public float alphaBeta (int depth, State s, float alpha, float beta){
+        if(env.is_terminal_state(s) || depth <= 0){
+            return heuristics.eval(s);
+        }
+        float bestValue = Float.NEGATIVE_INFINITY;
+
+        List<Action> actions = env.get_legal_actions(s);
+        for(Action a : actions){
+            s.doAction(a, true); // do action
+            float value = -alphaBeta(depth-1, s, -beta, -alpha);
+            s.doAction(a,false); // undo action
+            bestValue = Math.max(value, bestValue);
+            if(bestValue > alpha){
+                alpha = bestValue;
+                if(alpha >=beta)
+                    break;
+            }
+        }
+        return bestValue;
+    }
+}
--- src/EnvironmentTest.java
+++ src/EnvironmentTest.java
@@ -5,7 +5,7 @@ public class EnvironmentTest {
 
     Environment env;
     Heuristics heuristics;
-    MiniMax miniMax;
+    AlphaBeta alphaBeta;
 
     public static void main(String[] arg){
         new EnvironmentTest();
@@ -21,18 +21,23 @@ public class EnvironmentTest {
 //        Environment env = new Environment(width,height); // init like starting a new game
 
         HashSet<Coordinate> white_pawns = new HashSet<>();
-        white_pawns.add(new Coordinate(2,3));
+        white_pawns.add(new Coordinate(0,2));
+        white_pawns.add(new Coordinate(2,2));
+        white_pawns.add(new Coordinate(2,1));
 
         HashSet<Coordinate> black_pawns = new HashSet<>();
+        black_pawns.add(new Coordinate(0,4));
+        black_pawns.add(new Coordinate(2,4));
+        black_pawns.add(new Coordinate(0,3));
+        black_pawns.add(new Coordinate(2,3));
         black_pawns.add(new Coordinate(1,1));
-        black_pawns.add(new Coordinate(1,4));
 
 
         State s = new State(white_pawns, black_pawns, white_turns);
         env = new Environment(width,height, s);// init like start in a state situation
         heuristics = new SimpleHeuristics();
         heuristics.init(env);
-        miniMax = new MiniMax(env, heuristics);
+        alphaBeta = new AlphaBeta(env, heuristics);
 
         System.out.println("aktueller Status:");
         System.out.println(env.getCurrentState());
@@ -47,7 +52,7 @@ public class EnvironmentTest {
         Action nextAction = null;
         int max_depth = 3;
         for(int i = 0; i < max_depth; i++){
-            nextAction = miniMax.miniMaxRoot(i, env.currentState);
+            nextAction = alphaBeta.alphaBetaRoot(i, env.currentState, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
         }
         System.out.println();
         if(nextAction != null){
@@ -56,8 +61,8 @@ public class EnvironmentTest {
             System.out.println(env.get_next_State(env.getCurrentState(), nextAction));
             System.out.println();
 
-            System.out.println("war letzter Zug:" + env.is_terminal_action(env.getCurrentState(), actions.get(0)));
-            System.out.println("neuer Spielstatus:" + env.is_winning_move(env.getCurrentState(), actions.get(0)));
+            System.out.println("war letzter Zug:" + env.is_terminal_action(env.getCurrentState(), nextAction));
+            System.out.println("neuer Spielstatus:" + env.is_winning_move(env.getCurrentState(), nextAction));
         }
     }
 }
